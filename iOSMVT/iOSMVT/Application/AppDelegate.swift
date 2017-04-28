f2c4ba0cb37bf77fac5671c70574ef62ec3ca0db
lihy <liheyuan.bj@gmail.com>
2017-04-28 14:28:56 +0800

    MOD: Version 0.8
    1. app icon
    2. api loading animating support
    3. splash screen

--- iOSMVT/iOSMVT/Application/AppDelegate.swift
+++ iOSMVT/iOSMVT/Application/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UIApplication.shared.isStatusBarHidden = false
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.backgroundColor = UIColor.white
         self.window?.makeKeyAndVisible()
